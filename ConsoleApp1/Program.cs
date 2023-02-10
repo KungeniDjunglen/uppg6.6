2d4c7384c3ed205fd23d7a9783be335ff5f4561b
Hugge25 <111962627+KungeniDjunglen@users.noreply.github.com>
2023-02-10 10:12:35 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void StorSmåBokstav(string text)
+    {
+        string[] ord = text.Split(' ');
+        for (int i = 0; i < ord.Length; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.Write(ord[i].ToLower());
+            }
+            else
+            {
+                Console.Write(ord[i].ToUpper());
+            }
+            Console.Write(" ");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv en mening:");
+        StorSmåBokstav(Console.ReadLine());
+    }
+}
